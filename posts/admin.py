bab251e48b01f4b97a0261f5b7a791e4e5c4a92d
George <georgenechyporenko@gmail.com>
2021-05-18 12:33:27 -0700

    inital commit

---
+++ posts/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+#my imports
+from posts.models import Post
+
+# Register your models here.
+admin.site.register(Post)
+
---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+# my imports
+
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
